95e45d8178be8c5f7def5265fd76ece4bbd6dbff
Anton Ohorodnyk <anton@ohorodnyk.name>
2022-04-20 09:51:18 -0700

    Format list of points

--- linkedlist/doc.go
+++ linkedlist/doc.go
@@ -5,10 +5,10 @@ The Singly linked list's node contains only 1 link to the next value.
 The Doubly linked list's node contains two links to the next and previous value.
 
 Every linked list has four different implementations:
-1. Comparable - the linked list that supports comparable types
-2. ComparablySync - the Comparable implementation with concurrent access
-3. Any - the linked list that supports any types, but compare them in search by value methods though reflect.DeepEqual method or a custom comparable function can be passed
-4. AnySync - the Any implementation with concurrent access
+	1. Comparable - the linked list that supports comparable types
+	2. ComparablySync - the Comparable implementation with concurrent access
+	3. Any - the linked list that supports any types, but compare them in search by value methods though reflect.DeepEqual method or a custom comparable function can be passed
+	4. AnySync - the Any implementation with concurrent access
 
 Any and Comparable implementations are needed to speed-up the implementation of the linked list's search methods.
 The comparable implementation in 16 times faster than the Any implementation in the search methods with comparable types.
