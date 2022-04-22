cec029456e6c57b9099a60c8d4e43e17d29b0dc4
Anton Ohorodnyk <anton@ohorodnyk.name>
2022-04-21 22:53:28 -0700

    #9 Copy ordered types

--- math/comparison.go
+++ math/comparison.go
@@ -1,18 +1,13 @@
 package math
 
-// Comparable is a golang type list that are related to math comparison operations.
-// All types have ~ prefix to work with custom types that created from the listed types.
-// This interface is public to be used in other packages for custom impleentations.
-type Comparable interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr | ~float32 | ~float64
-}
+import "github.com/aohorodnyk/stl/types"
 
 // MaxMulti is a function that returns the maximum value for the more than 2 values in a row.
 // By using this functions we can compare from 0 to any number of values.
 // This function returns 0 if there is no values in the input.
 // If you surely know that you need to compare only two values, use Max instead.
 // MaxMulti function is a little bit slower than Max function.
-func MaxMulti[T Comparable](input ...T) T {
+func MaxMulti[T types.Ordered](input ...T) T {
 	cmp := func(first, second T) bool {
 		return first > second
 	}
@@ -25,7 +20,7 @@ func MaxMulti[T Comparable](input ...T) T {
 // This function returns 0 if there is no values in the input.
 // If you surely know that you need to compare only two values, use Min instead.
 // MinMulti function is a little bit slower than Min function.
-func MinMulti[T Comparable](input ...T) T {
+func MinMulti[T types.Ordered](input ...T) T {
 	cmp := func(first, second T) bool {
 		return first < second
 	}
@@ -35,7 +30,7 @@ func MinMulti[T Comparable](input ...T) T {
 
 // Min is a function that returns the minimum value for the two values.
 // This function is blazing fast.
-func Min[T Comparable](first, second T) T {
+func Min[T types.Ordered](first, second T) T {
 	if first < second {
 		return first
 	}
@@ -45,7 +40,7 @@ func Min[T Comparable](first, second T) T {
 
 // Max is a function that returns the minimum value for the two values.
 // This function is blazing fast.
-func Max[T Comparable](first, second T) T {
+func Max[T types.Ordered](first, second T) T {
 	if first > second {
 		return first
 	}
