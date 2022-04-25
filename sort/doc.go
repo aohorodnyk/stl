f7858da2a1e284f2a8918fe7c8029ad774aa5671
Anton Ohorodnyk <anton@ohorodnyk.name>
2022-04-24 22:14:19 -0700

    #20 Add short documentations for the main package

---
+++ sort/doc.go
@@ -0,0 +1,5 @@
+/*
+Sort package provides simplified interfaces to sort functions with various types.
+Sort functions in this package sort slices of any type that implements the sort.Interface and all types.Ordered types.
+*/
+package sort
