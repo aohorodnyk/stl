f2798f032538fdb322d013bc329cf0a98b0b9e1c
Anton Ohorodnyk <anton@ohorodnyk.name>
2022-04-26 10:15:57 -0700

    #24 Add few lines to all packages with small doc.go(s)

---
+++ collections/cache/doc.go
@@ -0,0 +1,5 @@
+/*
+List of cache collections that implement cache interface.
+This package provides different types of caches that will help to improve performance in all applications.
+*/
+package cache
