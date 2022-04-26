bba53151edd8b2e9780dcdcbbae3b8ef1d6a05d4
Anton Ohorodnyk <anton@ohorodnyk.name>
2022-04-26 10:15:57 -0700

    #24 Add few lines to all packages with small doc.go(s)

---
+++ collections/cache/lru/doc.go
@@ -0,0 +1,6 @@
+/*
+LRU algorithm is a cache replacement algorithm.
+LRU is a type of cache that stores recently used items in the cache and discards the least recently used items.
+This package provides different implementations of the LRU algorithm.
+*/
+package lru
---
+++ collections/doc.go
@@ -0,0 +1,5 @@
+/*
+List of collections that implement the main basic data structures.
+This package can be used to implement more complex data structures and algorithms.
+*/
+package collections
