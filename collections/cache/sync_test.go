0260ef2eaed8e3c982840c421d4cc390bd2952de
Anton Ohorodnyk <anton@ohorodnyk.name>
2022-04-30 23:19:33 -0700

    #31 Add a simple example for cache sync

--- collections/cache/sync_test.go
+++ collections/cache/sync_test.go
@@ -1,5 +1,21 @@
 package cache_test
 
-import "github.com/aohorodnyk/stl/collections/cache"
+import (
+	"fmt"
+
+	"github.com/aohorodnyk/stl/collections/cache"
+	"github.com/aohorodnyk/stl/collections/cache/lru"
+)
 
 var _ cache.Cache[int, int] = &cache.Sync[int, int]{}
+
+func ExampleSync() {
+	c := cache.NewSync[string, string](lru.NewSimple[string, string](10))
+	c.Set("key", "value")
+
+	value, ok := c.Value("key")
+	fmt.Println(value, ok)
+
+	// Output:
+	// value true
+}
