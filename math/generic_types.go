137ea2700f140d05dc1fd37b10ea3595939e9225
Anton Ohorodnyk <anton@ohorodnyk.name>
2022-05-01 08:35:28 -0700

    [main] Remove not needed generic_types from math package

--- math/generic_types.go
+++
@@ -1,8 +0,0 @@
-package math
-
-// Comparable is a golang type list that are related to math comparison operations.
-// All types have ~ prefix to work with custom types that created from the listed types.
-// This interface is public to be used in other packages for custom impleentations.
-type Comparable interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr | ~float32 | ~float64
-}
