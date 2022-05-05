36a137eb07bb7a4bc27e07a282554c73ea39b41c
Anton Ohorodnyk <anton@ohorodnyk.name>
2022-05-05 09:26:32 -0700

    #42 Add Compare function and rename CompareMulti to MinMaxMulti

--- math/comparison.go
+++ math/comparison.go
@@ -12,7 +12,7 @@ func MaxMulti[T types.Ordered](input ...T) T {
 		return first > second
 	}
 
-	return CompareMulti(cmp, input...)
+	return MinMaxMulti(cmp, input...)
 }
 
 // MaxMulti is a function that returns the minimum value for the more than 2 values in a row.
@@ -25,7 +25,7 @@ func MinMulti[T types.Ordered](input ...T) T {
 		return first < second
 	}
 
-	return CompareMulti(cmp, input...)
+	return MinMaxMulti(cmp, input...)
 }
 
 // Min is a function that returns the minimum value for the two values.
@@ -48,11 +48,11 @@ func Max[T types.Ordered](first, second T) T {
 	return second
 }
 
-// CompareMulti is a function that returns the minimum or maximum value for the 0..any number of values.
+// MinMaxMulti is a function that returns the minimum or maximum value for the 0..any number of values.
 // To use this function you must pass a function that returns true if the first value
 // is greater or lower than the second value.
 // This function can be used to compare custom structures or interfaces.
-func CompareMulti[T any](cmp func(T, T) bool, input ...T) T {
+func MinMaxMulti[T any](cmp func(T, T) bool, input ...T) T {
 	var result T
 
 	if len(input) == 0 {
@@ -67,4 +67,21 @@ func CompareMulti[T any](cmp func(T, T) bool, input ...T) T {
 	}
 
 	return result
+}
+
+// Compare is a function that returns the compartison result of two values.
+// It's a spaceship operator '<=>' implementation.
+// If the source value is equal to the target value, the function returns 0.
+// If the source value is greater than the target value, the function returns 1.
+// If the source value is less than the target value, the function returns -1.
+func Compare[T types.Ordered](source, target T) int {
+	if source < target {
+		return -1
+	}
+
+	if source > target {
+		return 1
+	}
+
+	return 0
 }
