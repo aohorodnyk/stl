3821af508d0b4c412f9f3b977b2faff1f18ba401
Anton Ohorodnyk <anton@ohorodnyk.name>
2022-06-20 12:38:33 -0700

    [main] Replace exhaustivestruct to exhaustruct in .golangci.tml

--- collections/binflags/fixed.go
+++ collections/binflags/fixed.go
@@ -7,22 +7,22 @@ import (
 	"github.com/aohorodnyk/stl/types"
 )
 
-// Static is a static size implementation of the binflags.
+// Fixed is a fixed size implementation of the binflags.
 // This is a slice that is allocated to collect flags in bits.
 // The max amount of flags can be stored per slice can be calculated by the formula: `slice length` * `bits in byte`.
 //
 // This implementation can be used whenever you want to have a predictable size and performance.
 // Also it can be saved in a database as an array type.
 // To initialize the Dynamic type, use make function or usual instatination:
-//   flags := make(Static[uint], 3)
+//   flags := make(Fixed[uint], 3)
 // or
-//   flags := Static[uint]{0, 0, 0}
-type Static[T types.Integer] []T
+//   flags := Fixed[uint]{0, 0, 0}
+type Fixed[T types.Integer] []T
 
 // IsSet returns true if the flag is set.
 // If the flag is not set, then it returns false.
 // This function has O(1) complexity.
-func (s Static[T]) IsSet(bitNumber uint) bool {
+func (s Fixed[T]) IsSet(bitNumber uint) bool {
 	if len(s) == 0 {
 		return false
 	}
@@ -44,7 +44,7 @@ func (s Static[T]) IsSet(bitNumber uint) bool {
 // This function is idempotent.
 // This function has O(1) complexity.
 // It returns false if the flag is out of range.
-func (s Static[T]) Set(bitNumber uint) bool {
+func (s Fixed[T]) Set(bitNumber uint) bool {
 	if len(s) == 0 {
 		return false
 	}
@@ -67,7 +67,7 @@ func (s Static[T]) Set(bitNumber uint) bool {
 // This function is idempotent.
 // This function has O(1) complexity.
 // It returns false if the flag is out of range.
-func (s Static[T]) Unset(bitNumber uint) bool {
+func (s Fixed[T]) Unset(bitNumber uint) bool {
 	if len(s) == 0 {
 		return false
 	}
