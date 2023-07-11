6cd607b91d77e7ac77385fe5797a6fd8d3fe6bab
Anton Ohorodnyk <me@aohorodnyk.com>
2023-07-11 08:08:48 -0700

    #16 Rename `slices.IsSortReverse` to `slices.IsReversed`

--- slices/is_sorted.go
+++ slices/is_sorted.go
@@ -20,7 +20,7 @@ func IsSorted[T constraints.Ordered](slice []T) bool {
 // The function is reimplemented (does not use standard library),
 // to improve performance and decrease memory usage.
 // It works with O(1) memory and O(n) performance.
-func IsSortedReverse[T constraints.Ordered](slice []T) bool {
+func IsReversed[T constraints.Ordered](slice []T) bool {
 	for i := len(slice) - 1; i > 0; i-- {
 		if slice[i] > slice[i-1] {
 			return false
